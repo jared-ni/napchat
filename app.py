6b8e65c85f90d5944042a7f63a7bff75bba7a13b
Jared <milkteadj@gmail.com>
2021-12-07 03:34:49 -0500

    can't run on heroku

--- app.py
+++ app.py
@@ -850,5 +850,5 @@ def newnap():
 
 
 if __name__ == '__main__':
-  app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
-    
+  #app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+  app.run()
